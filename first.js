14328d25826265da06d22031d04b35c29da14405
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-09-25 11:44:05 +0530

    todays js practice

---
+++ first.js
@@ -0,0 +1,51 @@
+// console.log("hello priyesh this side")
+// alert("Something went wrong")
+// document.write("hello everybody")
+
+
+// a=parseInt(prompt("enter value"))
+// b=parseInt(prompt("enter another value"))
+
+// console.log(a+b)
+
+// console.log(a*b)
+
+// console.log(a/b)
+
+// console.log(a**3)
+
+// -------------------------------------------✅ Q2: Write a program to swap two numbers ----------------------------------------------------
+
+// let a = parseInt(prompt("enter number 1"))
+// let b = parseInt(prompt("enter number 2"))
+
+// console.log("The swapping number 1 is",a,"The swapping number two is ",b)
+
+// let temp = a ;
+// a=b
+// b=temp;
+
+// console.log("after swapping : a = ",a," , b = ",b);
+
+// ----------------------------------- ✅ Q3: Write a program to find the largest of three numbers-------------------------------------------------
+
+
+// a=parseInt(prompt("enter num 1"))
+// b=parseInt(prompt("enter num 2"))
+// c=parseInt(prompt("enter num 3"))
+
+// if (a>=b && a>=c){
+//     console.log(a,"is the largest number");
+// }
+// else if(b>a && b>c){
+//     console.log(b,"is the largest number");
+// }
+// else{
+//     console.log(c,"is the largest number");
+// }
+
+
+// ------------------------------------ ✅ Q4: Write a program to print the sum of first 10 natural numbers--------------------------------------------
+//using for loop
+// let sum=0
+
