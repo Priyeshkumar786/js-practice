d51d1765a83a5b55cf6ab940dc2e616bbb08cacc
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-11-30 20:29:49 +0530

    added todays work:

--- tt1.js
+++ tt1.js
@@ -56,4 +56,11 @@
 //       } catch {
 //         display.value = "Error";
 //       }
+
+//     function calculate() {
+//       try {
+//         display.value = eval(display.value);
+//       } catch {
+//         display.value = "Error";
+//       }
 //     }
