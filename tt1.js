943bf97d2aacfe258f589dab0718aa2efc06d322
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-09 19:37:30 +0530

    added todays tasks

--- tt1.js
+++ tt1.js
@@ -335,3 +335,26 @@
 //         display.value = "Error";
 //       }
 
+======simple calculator======
+
+//  let display = document.getElementById("display");
+
+//     function append(value) {
+//       display.value += value;
+//     }
+
+//     function clearDisplay() {
+//       display.value = "";
+//     }
+
+//     function deleteChar() {
+//       display.value = display.value.slice(0, -1);
+//     }
+
+//     function calculate() {
+//       try {
+//         display.value = eval(display.value);
+//       } catch {
+//         display.value = "Error";
+//       }
+
